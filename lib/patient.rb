2ae3e096946d0ac2f93dcc304d71ef3657f59a1c
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 02:33:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,9 +8,18 @@ class Patient
   
   def initialize(name)
     @name = name
+    @@all << self
   end
   
   def new_appointment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    appointments.collect{|appointment| appointment.doctor}
   end
 end
