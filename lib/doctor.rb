e087c6d433258a5f2d229c9e90c35cf14e68c9dd
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 02:28:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,5 +11,17 @@ class Doctor
     @@all << self
   end
   
+  def new_appointment(patient, date)
+    app = Appointment.new(patient, date)
+    app.doctor = self
+    app
+  end
   
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+    appointments.collect{|appointment| appointment.patient}
+  end
 end
