28657effdd1bea57f46eac27226f6ced33fad42a
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 01:57:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+  attr_accessor :name, :songs, :genres
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name)
+    Song.genre = genre
+    Song.artist = self
+    @songs << song
+    
+  end
+end
